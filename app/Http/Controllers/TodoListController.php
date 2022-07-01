959ad4eef1c240911d37cdd93108ae44df8f1048
poliinamenshikow <menshikovapg@gmail.com>
2022-07-01 14:04:24 +0700

    commiting my first laravel spa

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\listItem;
+
+class TodoListController extends Controller
+{
+    public function index()
+    {
+        return view('welcome', ['listItems' => ListItem::where('is_complete', 0)->get()]);
+    }
+
+    public function markComplete($id)
+    {
+        \Log::info($id);
+        $listItem = ListItem::find($id);
+        $listItem->is_complete = 1;
+        $listItem->save();
+        return redirect('/');
+    }
+
+    public function saveItem(Request $request)
+    {
+
+        $newListItem = new ListItem;
+        $newListItem->name = $request->listItem;
+        $newListItem->is_complete = 0;
+        $newListItem->save();
+
+        return redirect('/');
+    }
+}
